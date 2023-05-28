22d7800029c7ddc6f34f1f06e1088e3616f8e157
Ayan  Dutta <ayandutta.it@gmail.com>
2023-05-28 12:31:35 +0530

    Create datatypes.java

---
+++ 01_Basics/datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes{
+
+    public static void main(String args[]) {
+
+        //variables
+        String name = "Ayan";
+        int x = 10;
+        int y = 5;
+        int age= 17;
+        double price= 25.25;
+        age=18;
+        name="Ayan Dutta";
+        int c= 45;
+        int d= 65;
+        int sum= c+d;
+        int ans= (x*y)/(x-y);
+        System.out.println(sum);
+        System.out.print("My age is:");
+        System.out.print(age);
+        System.out.println(ans);
+    }
+}
