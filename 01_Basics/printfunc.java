5aabfb944a15f9d0c5db83a52479374fc11c0f4a
Ayan  Dutta <ayandutta.it@gmail.com>
2023-05-28 12:27:10 +0530

    Update printfunc.java

---
+++ 01_Basics/printfunc.java
@@ -0,0 +1,25 @@
+public class printfunc{
+    public static void main(String args[]) {
+        //output
+
+        //println- prints new line
+        // "/n"-prints in new line
+            System.out.println("Hello my name is AYAN\nnice\nto meet you");
+         
+
+
+        //print- normally prints output
+            System.out.println("Hello my name is AYAN");
+
+
+
+        //Question 1
+        //Print the pattern 
+        //* 
+        //** 
+        //***
+        //****
+            System.out.print("*\n**\n***\n****");
+    }   
+}
+
